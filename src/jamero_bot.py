dd7650d935819c4cce521d2a2f4075f2cddae9a2
Pedro Mota <pjdrm@ist.utl.pt>
2019-07-07 20:51:25 +0100

    Avoiding tournament duplicates in create command.

--- src/jamero_bot.py
+++ src/jamero_bot.py
@@ -66,7 +66,7 @@ class JameroBot():
     async def click_button(self, xp_query, browser=None):
         if browser is None:
             browser = self.browser
-        button = self.browser.find_elements_by_xpath(xp_query)
+        button = browser.find_elements_by_xpath(xp_query)
         i = 0
         while len(button) == 0:
             await asyncio.sleep(1)
@@ -145,8 +145,6 @@ class JameroBot():
                            hour,
                            min,
                            period):
-        town = self.get_town(lobby_name)
-        tourn_name += " ("+town+" "+self.season+" - "+lobby_name.replace(town+"-", "").replace("-", " ")+")"
         await self.go_to_admin_page()
         host_tourn_xp = '//*[@id="content"]/div[2]/div/div/div/div/div[1]/a'
         await self.click_button(host_tourn_xp)
@@ -242,8 +240,9 @@ class JameroBot():
                     return True, msg
         return True, None
     
-    async def update_lobby_round_status(self, lobby_chan_id):
-        tourn_url = self.tourn_lobby_dict[lobby_chan_id]["tourn_url"]
+    async def update_lobby_round_status(self, tourn_name):
+        lobby_chan_id = self.tourn_lobby_dict[tourn_name]["chan_id"]
+        tourn_url = self.tourn_lobby_dict[tourn_name]["url"]
         lobby_channel = self.bot.get_channel(lobby_chan_id)
         print("Round Status %s" % lobby_channel.name)
         round_i, round_status = self.get_round_state(tourn_url)
@@ -251,7 +250,7 @@ class JameroBot():
         new_round, round_pin = await self.is_new_round(round_i, lobby_channel)
         if new_round:
             print("Shouting new round!")
-            await lobby_channel.send(self.tourn_lobby_dict[lobby_channel.id]["tag_role"]+" new round is up!")
+            await lobby_channel.send(self.tourn_lobby_dict[tourn_name]["tag_role"]+" new round is up!")
             if round_pin is not None:
                 await round_pin.delete()
             msg = await lobby_channel.send(embed=pairings_embed)
@@ -262,8 +261,9 @@ class JameroBot():
           
     async def check_round_status(self):
         while True:
-            for lobby_chan_id in self.tourn_lobby_dict:
-                await self.update_lobby_round_status(lobby_chan_id)
+            for tourn_name in self.tourn_lobby_dict:
+                if self.tourn_lobby_dict[tourn_name]["status"] == "ongoing":
+                    await self.update_lobby_round_status(tourn_name)
             await asyncio.sleep(self.check_frequency)
     
     async def go_to_admin_page(self):
@@ -272,17 +272,6 @@ class JameroBot():
         comm_button_xp = '//*[@id="navbar"]/ul/li[1]/ul/li[2]/a'#'//*[@id="navbar"]/ul/li[1]/ul/li[3]'
         await self.click_button(comm_button_xp)
     
-    async def get_tourn_info(self):
-        await self.go_to_admin_page()
-        tourn_info = {}
-        ongoing_tourn_xp = '//*[@class="tournamentWrap panel panel-dark active"][.//*[@style="color:green;font-size: 16px;line-height: 7px;"]]'
-        tourn_panels = self.browser.find_elements_by_xpath(ongoing_tourn_xp)
-        for panel in tourn_panels:
-            tourn_url = panel.find_elements_by_xpath('.//*[@class="btn btn-success"]')[0].get_attribute("href")
-            tourn_name = panel.find_elements_by_xpath('.//*[@class="tournamentName"]')[0].text.lower()
-            tourn_info[tourn_name] = tourn_url
-        return tourn_info
-        
     def is_tourn_lobby(self, channel_name):
         split_str = channel_name.split("-")
         if len(split_str) == 0:
@@ -313,22 +302,41 @@ class JameroBot():
                 if self.is_tourn_lobby(channel.name):
                     tourn_lobbies[channel.name] = channel.id
         return tourn_lobbies
-        
+    
+    def get_tourn_info(self, xp_query, tourn_info_dict, tourn_status):
+        tourn_panels = self.browser.find_elements_by_xpath(xp_query)
+        for panel in tourn_panels:
+            tourn_url = panel.find_elements_by_xpath('.//*[@class="btn btn-success"]')[0].get_attribute("href")
+            tourn_name = panel.find_elements_by_xpath('.//*[@class="tournamentName"]')[0].text.lower()
+            tourn_info_dict[tourn_name] = {}
+            tourn_info_dict[tourn_name]["url"] = tourn_url
+            tourn_info_dict[tourn_name]["status"] = tourn_status
+            
+    async def load_tourn_info(self):
+        await self.go_to_admin_page()
+        tourn_info = {}
+        ongoing_tourn_xp = '//*[@class="tournamentWrap panel panel-dark active"][.//*[@style="color:green;font-size: 16px;line-height: 7px;"]]'
+        self.get_tourn_info(ongoing_tourn_xp, tourn_info, "ongoing")
+        ongoing_tourn_xp = '//*[@class="tournamentWrap panel panel-dark active"][.//*[@style="color:orange;font-size: 16px;line-height: 7px;"]]'
+        self.get_tourn_info(ongoing_tourn_xp, tourn_info, "await-start")
+        return tourn_info
+    
     async def set_lobby_url_map(self):
-        tourn_info = await self.get_tourn_info()
-        
-        self.tourn_lobby_dict = {}
-        tag_role_dict = self.get_tourn_lobby_tag_roles()
-        for tourn_url in tourn_info:
+        self.tourn_lobby_dict = await self.load_tourn_info()
+        self.tag_role_dict = self.get_tourn_lobby_tag_roles()
+        for tourn_name in self.tourn_lobby_dict:
             for channel_name in self.tourn_lobbies_channels:
-                if channel_name in tourn_url:
-                    town = self.get_town(channel_name)
+                town = self.get_town(channel_name)
+                league = channel_name.split(town+"-")[0].replace("-", " ")
+                if town in tourn_name and league in tourn_name:
                     chan_id = self.tourn_lobbies_channels[channel_name]
-                    tag_role = tag_role_dict[town]
-                    self.tourn_lobby_dict[chan_id] = {"tourn_url": tourn_info[tourn_url],
-                                                      "tag_role": tag_role}
+                    tag_role = "TEST"#self.tag_role_dict[town]
+                    self.tourn_lobby_dict[tourn_name]["tag_role"] = tag_role
+                    self.tourn_lobby_dict[tourn_name]["chan_id"] = chan_id
                     break
-        
+    def add_tourn(self, tourn_name, url, chan_id, tag_role, tourn_status):
+        self.tourn_lobby_dict[tourn_name] = {"url": url, "chan_id": chan_id, "tag_role": tag_role, "status": tourn_status}
+    
     def run_discord_bot(self):
         @self.bot.event
         async def on_ready():
@@ -336,8 +344,8 @@ class JameroBot():
             self.tourn_lobbies_channels = await self.get_tourn_lobbies()
             await self.sa_login()
             print('Logged in Silph Arena')
-            #await self.set_lobby_url_map()
-            #print('Got tournament info')
+            await self.set_lobby_url_map()
+            print('Got tournament info')
             #self.bot.loop.create_task(self.check_round_status())
         
         @self.bot.command(pass_context=True)
@@ -365,17 +373,17 @@ class JameroBot():
                      tournament_type,
                      tourn_name,
                      date_str):
-            
+            print("Got create tournament command")
             if lobby_name not in self.tourn_lobbies_channels:
-                await ctx.message.channel.send("ERROR: Invalid lobby channel type `"+lobby_name+"`")
+                await ctx.message.channel.send("**ERROR**: Invalid lobby channel type `"+lobby_name+"`")
                 return
             tournament_type = tournament_type.title()
             month, day, hour, min, period = self.parse_date(date_str)
             
             if month is None:
-                await ctx.message.channel.send("ERROR: Invalid `date`. The format must be the following: `<mm>/<dd> <hh>:<mm><period>`")
+                await ctx.message.channel.send("**ERROR**: Invalid `date`. The format must be the following: `<mm>/<dd> <hh>:<mm><period>`")
             elif min not in MINUTE_INDEX:
-                error_msg = "ERROR: Invalid minute value in `date`. Valid minute values are: "
+                error_msg = "**ERROR**: Invalid minute value in `date`. Valid minute values are: "
                 mins_list = list(MINUTE_INDEX.keys())
                 mins_list.sort()
                 for valid_min in mins_list:
@@ -383,6 +391,14 @@ class JameroBot():
                 error_msg = error_msg[:-2]
                 await ctx.message.channel.send(error_msg)
             else:
+                town = self.get_town(lobby_name)
+                tourn_name += " ("+town+" "+self.season+" - "+lobby_name.replace(town+"-", "").replace("-", " ")+")"
+                if tourn_name in  self.tourn_lobby_dict:
+                    warn_msg = "**WARNING**: tournament `"+\
+                                tourn_name+"` already exists and was **NOT** created: <"+\
+                                self.tourn_lobby_dict[tourn_name]["url"]+">"
+                    await ctx.message.channel.send(warn_msg)
+                    return
                 checkin_code, tourn_id = await self.create_tourn(lobby_name,
                                                                 tournament_type,
                                                                 tourn_name,
@@ -392,9 +408,12 @@ class JameroBot():
                                                                 min,
                                                                 period)
                 if checkin_code is None: #TODO: allow handling different type of errors
-                    await ctx.message.channel.send("ERROR: Invalid tournament type `"+tournament_type+"`")
+                    await ctx.message.channel.send("**ERROR**: Invalid tournament type `"+tournament_type+"`")
                 else:
-                    await ctx.message.channel.send("Created tournament:\n\tLobby: %s\n\turl: https://silph.gg/t/%s\n\tcheck-in code: %s"%(lobby_name, tourn_id, checkin_code))
+                    url = "https://silph.gg/t/"+tourn_id
+                    tag_role = "TEST"#self.tag_role_dict[town]
+                    self.add_tourn(tourn_name, url, self.tourn_lobbies_channels[lobby_name], tag_role, "await-start")
+                    await ctx.message.channel.send("Created tournament:\n\tLobby: %s\n\turl: <%s>\n\tcheck-in code: %s"%(lobby_name, url, checkin_code))
             
         self.bot.run(self.bot_token)
         
